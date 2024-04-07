4530ef5eb1108d6419fd5231c95d56086f682741
METR1CKA <ferchosalazar054@gmail.com>
2024-04-07 12:45:00 -0600

    fix(v2.15.34): properties fixed

--- database/migrations/WithForeignTables/System/2024_03_29_233302_create_enterprises_table.php
+++ database/migrations/WithForeignTables/System/2024_03_29_233302_create_enterprises_table.php
@@ -17,8 +17,8 @@ return new class extends Migration {
       $table->string('contact_name', 100)->nullable(false);
       $table->string('contact_phone', 15)->nullable(false);
       $table->string('contact_email', 200)->nullable(false);
-      $table->string('lega_id', 200)->nullable(false);
-      $table->string('lega_name', 200)->nullable(false);
+      $table->string('legal_id', 200)->nullable(false);
+      $table->string('legal_name', 200)->nullable(false);
       $table->boolean('active')
         ->nullable(false)
         ->default(true);
