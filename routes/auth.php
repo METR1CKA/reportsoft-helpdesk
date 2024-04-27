df437195153cb705ec85abdac9145f1d26bd03ca
METR1CKA <ferchosalazar054@gmail.com>
2024-04-27 16:24:50 -0600

    fix(v2.19.46): fix

--- routes/auth.php
+++ routes/auth.php
@@ -27,8 +27,8 @@ Route::middleware([
   Route::get('login', [AuthenticatedSessionController::class, 'create'])
     ->name('login');
 
-  Route::post('login', [AuthenticatedSessionController::class, 'store'])
-    ->middleware('check-access');
+  Route::post('login', [AuthenticatedSessionController::class, 'store']);
+  // ->middleware('check-access');
 
   // Forgot password
   Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
